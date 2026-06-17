77f82ff8df96d6d22cc88c9060ce6b6b21b79a6f
Rishabh <rishavvyas74@gmail.com>
2026-06-17 12:56:34 +0530

    Added more Visa Types

--- src/data/visaTypes.js
+++ src/data/visaTypes.js
@@ -1,7 +1,11 @@
 const visaTypes = [
   "Tourist Visa",
   "Student Visa",
-  "Work Visa"
+  "Work Visa",
+  "Permanent Residency",
+  "Business Visa",
+  "Family Sponsorship Visa",
+  "Investor Visa"
 ];
 
 export default visaTypes;
